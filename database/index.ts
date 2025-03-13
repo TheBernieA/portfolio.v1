1cf13ca7ae4b263ec244b141d6b63e351d99981d
Bernard <bernardamankwah@hotmail.com>
2025-03-13 15:24:23 +0100

    added react native

--- database/index.ts
+++ database/index.ts
@@ -47,30 +47,6 @@ export const links = [
   { name: "linkedin", url: "https://www.linkedin.com/in/theberniea/" },
 ];
 
-export const experiences: any = {
-  "Tech Fusion": {
-    title: " Founder & Lead Full-Stack Developer @ Tech Fusion",
-    companyLink: "",
-    date: "November 2024 - Present",
-    details: [
-      "Developed features and implemented end-to-end testing for a SaaS reporting and auditing platform.",
-      "Created accessible, efficient, and secure user interfaces for various platforms.",
-      "Led the development of multi-domain web applications with features such as internationalization, localization, secure authentication, and CSV data processing, delivering simple yet sophisticated UI solutions.",
-    ],
-  },
-  Develhope: {
-    title: "Frontend Engineer and Tutor Assistant/Lead @ Develhope",
-    companyLink: "https://www.develhope.co/",
-    date: "december 2021 - december 2024",
-    details: [
-      "Developed 10+ responsive UIs in Angular/React, boosting user engagement via A/B testing.",
-      "Documented APIs and codebases using Swagger and JSDoc, accelerating onboarding for new developers.",
-      "Conducted code reviews, reducing runtime errors through Jest and ESLint integration.",
-      "Mentored 20+ students in React, Agular, Node.js, TypeScript, and AWS, achieving a 95% course completion rate. ",
-    ],
-  },
-};
-
 export const frontend = [
   "HTML5",
   "CSS3",
@@ -79,6 +55,7 @@ export const frontend = [
   "Javascript ES6+",
   "RxJS",
   "Redux",
+  "React Native",
   "Ngrx",
   "Angular > 12",
   "Reactjs",
@@ -98,42 +75,4 @@ export const tools = [
   "DigitalOcean",
   "vscode",
   "figma",
-];
-
-export const otherProjects = [
-  {
-    gitHubLink: "https://github.com/TheBernieA/portfolio.v1",
-    externalLink: "http://bernardamankwah.vercel.app/",
-    title: "Personal Website",
-    description: `My first portfolio website, built using Next.js and Framer Motion for animations. This project was a great learning experience, helping me understand the power of Next.js for performance optimization and how Framer Motion makes creating smooth, interactive animations effortless. It was a fun and insightful journey into building modular and dynamic web experiences.`,
-    tools: ["nextjs", "tailwind", "motion framer", "vercel"],
-  },
-  {
-    gitHubLink: "https://github.com/TheBernieA/CRUD-API",
-    externalLink: "",
-    title: "CRUD API",
-    description: `I developed a CRUD API using Node.js and Express, which was a valuable learning experience in backend development. This project helped me understand how controllers structure an application and how authentication mechanisms work to secure user data. Through this process, I gained hands-on experience with RESTful principles, middleware, and user authentication strategies, strengthening my backend development skills.`,
-    tools: ["nodejs", "express", "multer", "jwt"],
-  },
-  {
-    gitHubLink: "https://github.com/TheBernieA/tradebot",
-    externalLink: "",
-    title: "Trade execution system",
-    description: `I build a trading execution system which allows users to place as many trades they want to place at the click of a button. Although my knowledge in trading is very low, I was able to bring this idea to life and I had a lot of fun doing it.`,
-    tools: ["python", "javascript", "html", "css", "Flask", "MetaTrader5"],
-  },
-  {
-    gitHubLink: "https://github.com/TheBernieA/todo-angular-19",
-    externalLink: "",
-    title: "Todo",
-    description: `I build a simple todo app using angular 19 to explore the lastest features angular has to offer with my main focus on understanding how signals work.`,
-    tools: ["angular 19", "angular signals", "typescript"],
-  },
-  {
-    gitHubLink: "",
-    externalLink: "https://fancy-pony-c2738c.netlify.app/",
-    title: "a nike landing page",
-    description: `A sleek and fully responsive landing page web application built with React.js and styled using Tailwind CSS. This project not only enhanced my understanding of Tailwind's utility-first approach but also deepened my expertise in creating modern, responsive user interfaces.`,
-    tools: ["reactjs", "tailwind CSS", "typescript", "netlify"],
-  },
 ];
---
+++ database/otherProject.ts
@@ -0,0 +1,44 @@
+export const otherProjects = [
+  {
+    gitHubLink: "https://github.com/TheBernieA/portfolio.v1",
+    externalLink: "http://bernardamankwah.vercel.app/",
+    title: "Personal Website",
+    description: `My first portfolio website, built using Next.js and Framer Motion for animations. This project was a great learning experience, helping me understand the power of Next.js for performance optimization and how Framer Motion makes creating smooth, interactive animations effortless. It was a fun and insightful journey into building modular and dynamic web experiences.`,
+    tools: ["nextjs", "tailwind", "motion framer", "vercel"],
+  },
+  {
+    gitHubLink: "https://github.com/TheBernieA/CRUD-API",
+    externalLink: "",
+    title: "CRUD API",
+    description: `I developed a CRUD API using Node.js and Express, which was a valuable learning experience in backend development. This project helped me understand how controllers structure an application and how authentication mechanisms work to secure user data. Through this process, I gained hands-on experience with RESTful principles, middleware, and user authentication strategies, strengthening my backend development skills.`,
+    tools: ["nodejs", "express", "multer", "jwt"],
+  },
+  {
+    gitHubLink: "https://github.com/TheBernieA/tradebot",
+    externalLink: "",
+    title: "Trade execution system",
+    description: `I build a trading execution system which allows users to place as many trades they want to place at the click of a button. Although my knowledge in trading is very low, I was able to bring this idea to life and I had a lot of fun doing it.`,
+    tools: ["python", "javascript", "html", "css", "Flask", "MetaTrader5"],
+  },
+  {
+    gitHubLink: "https://github.com/TheBernieA/todo-angular-19",
+    externalLink: "",
+    title: "Todo",
+    description: `I build a simple todo app using angular 19 to explore the lastest features angular has to offer with my main focus on understanding how signals work.`,
+    tools: ["angular 19", "angular signals", "typescript", "CSS"],
+  },
+  {
+    gitHubLink: "https://github.com/TheBernieA/movie_app_native",
+    externalLink: "",
+    title: "React Native Movies App",
+    description: `Created a simple mobile app using react native (expo) to better understand how mobile app creation with react native works using themoviedb api to make api calls to fetch my database and appwrite to store search data to display most searched movies to display a trending movies section `,
+    tools: ["react native", "appwrite", "typescript", "tailwindCSS"],
+  },
+  {
+    gitHubLink: "",
+    externalLink: "https://fancy-pony-c2738c.netlify.app/",
+    title: "a nike landing page",
+    description: `A sleek and fully responsive landing page web application built with React.js and styled using Tailwind CSS. This project not only enhanced my understanding of Tailwind's utility-first approach but also deepened my expertise in creating modern, responsive user interfaces.`,
+    tools: ["reactjs", "tailwind CSS", "typescript", "netlify"],
+  },
+];
